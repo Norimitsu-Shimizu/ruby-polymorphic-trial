462e2cf8bcb7e2141e05690f6514cc0599b64110
Norimitsu-Shimizu <sparesound31@gmail.com>
2021-10-16 17:46:32 +0900

    feat: create family_restaurant table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_16_084150) do
+ActiveRecord::Schema.define(version: 2021_10_16_084506) do
+
+  create_table "family_restaurants", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "fish_markets", charset: "utf8mb4", force: :cascade do |t|
     t.string "fish_name"
