9977f7f820675c6cad5d1cbeacfd366b2144da07
Norimitsu-Shimizu <50897909+Norimitsu-Shimizu@users.noreply.github.com>
2021-10-17 20:30:33 +0900

    Revert "add association in markettrade"

--- app/models/market_trade.rb
+++ app/models/market_trade.rb
@@ -1,8 +1,4 @@
 class MarketTrade < ApplicationRecord
   belongs_to :sellar, polymorphic: true
   belongs_to :buyer, polymorphic: true
-  belongs_to :fish_market
-  belongs_to :family_restaurant
-  belongs_to :noodle_restaurant
-  belongs_to :sushi_restaurant
 end
