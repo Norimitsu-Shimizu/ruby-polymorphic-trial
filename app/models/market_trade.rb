4d70f8c3b3bd6ee177f8e7fba174bab71f78ac8f
Norimitsu-Shimizu <sparesound31@gmail.com>
2021-10-17 20:06:58 +0900

    feat: add association in markettrade

--- app/models/market_trade.rb
+++ app/models/market_trade.rb
@@ -1,4 +1,8 @@
 class MarketTrade < ApplicationRecord
   belongs_to :sellar, polymorphic: true
   belongs_to :buyer, polymorphic: true
+  belongs_to :fish_market
+  belongs_to :family_restaurant
+  belongs_to :noodle_restaurant
+  belongs_to :sushi_restaurant
 end
